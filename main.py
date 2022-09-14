bb9938dbc8081d269b309db3b1a71c460e750d54
yehouenoubk <yehouenoubk@educ.college-montreal.qc.ca>
2022-09-14 10:21:48 -0400

    now let's go

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Ce code a été fait par Karol Yehouenou du groupe 402
+Ce code cree une fonction qui prends en argument une phrase, et retourne le nombre de mots associè â la phrase à
+"""
+
+
+def count_word(word):
+
+    str(word)
+    number_word = len(word.split(" "))
+    return number_word
+
+
+print(count_word("Mon nom est Karol"))
